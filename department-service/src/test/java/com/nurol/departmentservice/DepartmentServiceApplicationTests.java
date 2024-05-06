59b4131a2d29830dbdf5b162fe908fbcccde58a8
cagatay aslan <aslann.cagatay@gmail.com>
2024-05-06 19:53:57 +0300

    create microservices

---
+++ department-service/src/test/java/com/nurol/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nurol.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
